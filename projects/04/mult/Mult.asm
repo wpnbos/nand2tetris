ee770ebf5abbcd4550ca4d311c1e30a2449983ad
William <william@unetiq.com>
2022-12-25 15:30:23 +0100

    Implement Mult

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -9,4 +9,27 @@
 // This program only needs to handle arguments that satisfy
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
-// Put your code here.
+    // Initialize R2 as zero
+    @R2
+    M = 0
+
+(LOOP)
+    // If (R1 == 0) -> END
+    @R1
+    D=M
+    @END
+    D;JEQ
+    // Add R0 to R2
+    @R0
+    D=M
+    @R2
+    M=D+M
+    // R1 = R1 - 1
+    @R1
+    M = M - 1
+    // goto LOOP
+    @LOOP
+    0;JMP
+(END)
+    @END
+    0;JMP
