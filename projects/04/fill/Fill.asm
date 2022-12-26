603faeacfeb23487eb8c3323e9f73b285a9a2978
William <william@unetiq.com>
2022-12-26 13:43:10 +0100

    Implement Fill

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -11,4 +11,77 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+// Loop that checks keyboard input
+(MONITOR)
+    // Reset counter
+    @8192
+    D=A
+    @counter
+    M=D
+
+    @KBD
+    D=M
+    // If nonzero, fill screen
+    @FILL
+    D;JNE
+    // Clear screen
+    @CLEAR
+    0;JMP
+
+(FILL)
+    // If (KBD == 0) -> MONITOR
+    @KBD
+    D=M
+    @MONITOR
+    D;JEQ
+
+    // If (counter == 0) -> MONITOR
+    @counter
+    D=M
+    @MONITOR
+    D;JEQ
+
+    // Fill pixel
+    @counter
+    D=M-1
+    @SCREEN
+    D=D+A
+    A=D
+    M=-1
+
+    // Decrease counter
+    @counter
+    M=M-1
+
+    // Loop
+    @FILL
+    0;JMP
+
+(CLEAR)
+    // If (KBD == 1) -> MONITOR
+    @KBD
+    D=M
+    @MONITOR
+    D;JNE
+
+    // If (counter == 0) -> MONITOR
+    @counter
+    D=M
+    @MONITOR
+    D;JEQ
+
+    // Fill pixel
+    @counter
+    D=M-1
+    @SCREEN
+    D=D+A
+    A=D
+    M=0
+
+    // Decrease counter
+    @counter
+    M=M-1
+
+    // Loop
+    @CLEAR
+    0;JMP
