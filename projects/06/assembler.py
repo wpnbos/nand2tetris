fb1c81778f067824bd96d344070029eb0d17187d
William <wpnbos@gmail.com>
2023-01-02 21:32:13 +0100

    Add test and implementation for parse_assembly

--- projects/06/assembler.py
+++ projects/06/assembler.py
@@ -1,2 +1,14 @@
+def parse_assembly(assembly_code: str) -> list[str]:
+    return [
+        stripped_line
+        for line in assembly_code.splitlines()
+        if (stripped_line := line.strip())
+    ]
+
+
+def assemble_instruction(instruction: str) -> str:
+    return ""
+
+
 def assemble(assembly_code: str) -> str:
     return
--- projects/06/test_assembler.py
+++ projects/06/test_assembler.py
@@ -1,11 +1,21 @@
 import pytest
 from pathlib import Path
 
-from assembler import assemble
+from assembler import assemble, parse_assembly
 
 
 hack_files_dir = Path(__file__).parent.joinpath("test").glob("*.hack")
 hack_files = list(hack_files_dir)
+
+
+def test_parse_assembly_returns_list_of_instruction_strings():
+    assembly_code = "@2\nD=A\n@3\nD=D+A\n"
+    assert parse_assembly(assembly_code) == ["@2", "D=A", "@3", "D=D+A"]
+
+
+def test_parse_assembly_strips_preceding_and_trailing_whitespace_from_instructions():
+    assembly_code = "\t@2\n\tD=A\n@3  \n\rD=D+A\n"
+    assert parse_assembly(assembly_code) == ["@2", "D=A", "@3", "D=D+A"]
 
 
 @pytest.mark.parametrize(
