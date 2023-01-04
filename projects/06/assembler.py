c4c14342d9652bc6dcc62c0da5cc85f1e1699762
William <wpnbos@gmail.com>
2023-01-04 20:38:09 +0100

    Add working implementation of assembler

--- projects/06/assembler.py
+++ projects/06/assembler.py
@@ -1,3 +1,6 @@
+from copy import deepcopy
+
+
 COMP_CODES = {
     "0": "101010",
     "1": "111111",
@@ -31,54 +34,101 @@ JMP_CODES = {
 }
 
 
+SYMBOLS = {
+    "SCREEN": 16384,
+    "KBD": 24576,
+}
+for i, symbol in enumerate(("SP", "LCL", "ARG", "THIS", "THAT")):
+    SYMBOLS[symbol] = i
+for i in range(16):
+    SYMBOLS[f"R{i}"] = i
+
+
+def get_label_symbols(lines: list[str]) -> dict[str, int]:
+    label_symbols = {}
+
+    line_counter = 0
+    for line in lines:
+        if line.startswith("("):
+            # Line is a label declaration
+            symbol = line.replace("(", "").replace(")", "")
+            label_symbols[symbol] = line_counter
+            continue
+        line_counter += 1
+
+    return label_symbols
+
+
+def clean_lines(lines: list[str]) -> list[str]:
+    """
+    Removes comments and empty lines.
+    """
+    return [
+        cleaned_line.strip() for line in lines if (cleaned_line := line.split("//")[0])
+    ]
+
+
+def assemble_c_instruction(instruction: str) -> str:
+    dest, comp, jmp = None, None, None
+    comp = instruction
+    if "=" in comp:
+        dest, comp = comp.split("=")
+    if ";" in comp:
+        comp, jmp = comp.split(";")
+    for part in (dest, comp, jmp):
+        if part:
+            part = part.strip()
+
+    dest_code = [0, 0, 0]
+    if dest:
+        dest = "".join(sorted(dest))
+        if "A" in dest:
+            dest_code[0] = 1
+        if "D" in dest:
+            dest_code[1] = 1
+        if "M" in dest:
+            dest_code[2] = 1
+    dest_code = "".join([str(bit_) for bit_ in dest_code])
+
+    comp_code = "000000"
+    if comp:
+        comp_code = COMP_CODES[comp.replace("M", "A")]
+        a = "1" if "M" in comp else "0"
+
+    jmp_code = "000"
+    if jmp:
+        jmp_code = JMP_CODES[jmp]
+
+    return f"111{a}{comp_code}{dest_code}{jmp_code}"
+
+
 def assemble(assembly_code: str) -> str:
     binary_codes = []
-    lines = assembly_code.splitlines()
+    lines = clean_lines(assembly_code.splitlines())
+    symbols = deepcopy(SYMBOLS) | get_label_symbols(lines)
+
+    address_counter = 16
     for line in lines:
-        print(line)
-        # Skip comments
-        line = line.strip()
-        if line.startswith("//") or not line:
+        if line.startswith("("):
+            # Skip label declarations
             continue
         if line.startswith("@"):
             # Line is an A-instruction
-            value = int(line[1:])
+            value = line[1:]
+            try:
+                value = int(value)
+            except ValueError:
+                # Instruction contains a symbol
+                if value not in symbols:
+                    symbols[value] = address_counter
+                    address_counter += 1
+                value = symbols[value]
             binary_value = format(value, "015b")
             binary_code = f"0{binary_value}"
             binary_codes.append(binary_code)
         else:
             # Line is a C-instruction
-            dest, comp, jmp = None, None, None
-            comp = line
-            if "=" in comp:
-                dest, comp = line.split("=")
-            if ";" in comp:
-                comp, jmp = comp.split(";")
-            for part in (dest, comp, jmp):
-                if part:
-                    part = part.strip()
-            print(f"{dest=}, {comp=}, {jmp=}")
-            dest_code = [0, 0, 0]
-            if dest:
-                dest = "".join(sorted(dest))
-                if "A" in dest:
-                    dest_code[0] = 1
-                if "D" in dest:
-                    dest_code[1] = 1
-                if "M" in dest:
-                    dest_code[2] = 1
-            dest_code = "".join([str(bit_) for bit_ in dest_code])
+            binary_codes.append(assemble_c_instruction(line))
 
-            comp_code = "000000"
-            if comp:
-                comp_code = COMP_CODES[comp.replace("M", "A")]
-                a = "1" if "M" in comp else "0"
-
-            jmp_code = "000"
-            if jmp:
-                jmp_code = JMP_CODES[jmp]
-
-            binary_codes.append(f"111{a}{comp_code}{dest_code}{jmp_code}")
     binary_code = "\n".join(binary_codes) + "\n"
-    print(binary_code)
     return binary_code
