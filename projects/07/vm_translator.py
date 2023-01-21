f9b2a6dac059e50ca1996d1973f35eb24ec30a1e
William <wpnbos@gmail.com>
2023-01-21 20:00:48 +0100

    Refactor vm_translator.py

--- projects/07/vm_translator.py
+++ projects/07/vm_translator.py
@@ -1,4 +1,4 @@
-INSTRUCTIONS = {"SP++": "@SP\nM=M+1", "SP--": "@SP\nM=M-1"}
+INSTRUCTIONS = {"SP++": "@SP\nM=M+1", "SP--": "@SP\nM=M-1", "SP=D": "@SP\nA=M\nM=D"}
 MEM_MAP = {
     "local": 1,
     "argument": 3,
@@ -26,16 +26,14 @@ def clean_lines(lines: list[str]) -> list[str]:
     ]
 
 
-def generate_push(value: int) -> str:
+def pop_to_d() -> str:
     return "\n".join(
         [
-            f"// push constant {value}",
-            f"@{value}",
-            "D=A",
-            "@SP",
+            INSTRUCTIONS["SP--"],
             "A=M",
-            "M=D",
-            INSTRUCTIONS["SP++"],
+            "D=M",
+            INSTRUCTIONS["SP--"],
+            "A=M",
         ]
     )
 
@@ -44,11 +42,7 @@ def generate_add() -> str:
     return "\n".join(
         [
             "// add",
-            INSTRUCTIONS["SP--"],
-            "A=M",
-            "D=M",
-            INSTRUCTIONS["SP--"],
-            "A=M",
+            pop_to_d(),
             "M=M+D",
             INSTRUCTIONS["SP++"],
         ]
@@ -70,11 +64,7 @@ def generate_and() -> str:
     return "\n".join(
         [
             "// and",
-            INSTRUCTIONS["SP--"],
-            "A=M",
-            "D=M",
-            INSTRUCTIONS["SP--"],
-            "A=M",
+            pop_to_d(),
             "M=M&D",
             INSTRUCTIONS["SP++"],
         ]
@@ -85,11 +75,7 @@ def generate_gt(comp_count: int) -> str:
     return "\n".join(
         [
             "// gt",
-            INSTRUCTIONS["SP--"],
-            "A=M",
-            "D=M",
-            INSTRUCTIONS["SP--"],
-            "A=M",
+            pop_to_d(),
             "D=D-M",
             f"@GT{comp_count}",
             "D;JLT",
@@ -112,11 +98,7 @@ def generate_lt(comp_count: int) -> str:
     return "\n".join(
         [
             "// lt",
-            INSTRUCTIONS["SP--"],
-            "A=M",
-            "D=M",
-            INSTRUCTIONS["SP--"],
-            "A=M",
+            pop_to_d(),
             "D=D-M",
             f"@LT{comp_count}",
             "D;JGT",
@@ -139,11 +121,7 @@ def generate_sub() -> str:
     return "\n".join(
         [
             "// sub",
-            INSTRUCTIONS["SP--"],
-            "A=M",
-            "D=M",
-            INSTRUCTIONS["SP--"],
-            "A=M",
+            pop_to_d(),
             "M=M-D",
             INSTRUCTIONS["SP++"],
         ]
@@ -154,11 +132,7 @@ def generate_or() -> str:
     return "\n".join(
         [
             "// or",
-            INSTRUCTIONS["SP--"],
-            "A=M",
-            "D=M",
-            INSTRUCTIONS["SP--"],
-            "A=M",
+            pop_to_d(),
             "M=M|D",
             INSTRUCTIONS["SP++"],
         ]
@@ -193,11 +167,7 @@ def generate_eq(comp_count: int) -> str:
     return "\n".join(
         [
             "// eq",
-            INSTRUCTIONS["SP--"],
-            "A=M",
-            "D=M",
-            INSTRUCTIONS["SP--"],
-            "A=M",
+            pop_to_d(),
             "D=M-D",
             f"@EQUAL{comp_count}",
             "D;JEQ",
@@ -228,6 +198,93 @@ generators = {
 COMPS = {"eq": generate_eq, "lt": generate_lt, "gt": generate_gt}
 
 
+def push_constant_to_d(value: int) -> str:
+    return f"@{value}\nD=A"
+
+
+def push_value_from_address_to_d(address: str) -> str:
+    return f"@{address}\nD=M"
+
+
+def push_value_from_mem_seg_to_d(mem_seg: str, i: int) -> str:
+    return "\n".join(
+        [
+            f"@{SYMBOLS[mem_seg]}",
+            "D=M",
+            f"@{i}",
+            "A=D+A",
+            "D=M",
+        ]
+    )
+
+
+def generate_push(line: str, file_stem: str) -> str:
+    _, mem_seg, dest = line.split(" ")
+    address = str(
+        MEM_MAP[mem_seg] + int(dest)
+        if mem_seg not in ("static", "constant")
+        else f"{file_stem}.{int(dest)}"
+    )
+    instruction = "\n".join(
+        [
+            f"// push {mem_seg} {dest}",
+            push_value_from_address_to_d(address)
+            if mem_seg in ("temp", "pointer", "static")
+            else push_constant_to_d(int(dest))
+            if mem_seg == "constant"
+            else push_value_from_mem_seg_to_d(mem_seg, int(dest)),
+            INSTRUCTIONS["SP=D"],
+            INSTRUCTIONS["SP++"],
+        ]
+    )
+    return instruction
+
+
+def generate_pop(line: str, file_stem: str) -> str:
+    _, mem_seg, dest = line.split(" ")
+    if mem_seg in ("temp", "pointer", "static"):
+        address = (
+            MEM_MAP[mem_seg] + int(dest)
+            if not mem_seg == "static"
+            else f"{file_stem}.{int(dest)}"
+        )
+        return "\n".join(
+            [
+                f"// pop {mem_seg} {dest}",
+                f"@{address}",
+                "D=A",
+                "@R13",
+                "M=D",
+                "@SP",
+                "M=M-1",
+                "A=M",
+                "D=M",
+                "@R13",
+                "A=M",
+                "M=D",
+            ]
+        )
+    else:
+        return "\n".join(
+            [
+                f"// pop {mem_seg} {dest}",
+                f"@{SYMBOLS[mem_seg]}",
+                "D=M",
+                f"@{dest}",
+                "D=D+A",
+                "@R13",
+                "M=D",
+                "@SP",
+                "M=M-1",
+                "A=M",
+                "D=M",
+                "@R13",
+                "A=M",
+                "M=D",
+            ]
+        )
+
+
 def translate(vm_code: str, file_stem: str) -> str:
     # Seems to be a problem with gt. See first gt call in StackTest.vm
     lines = clean_lines(vm_code.splitlines())
@@ -237,88 +294,9 @@ def translate(vm_code: str, file_stem: str) -> str:
     for line in lines:
         command = line.split(" ")[0]
         if command == "push":
-            _, mem_seg, dest = line.split(" ")
-            if mem_seg == "constant":
-                output.append(generate_push(int(dest)))
-            # Write push for other mem segs than constant
-            elif mem_seg in ("temp", "pointer", "static"):
-                address = (
-                    MEM_MAP[mem_seg] + int(dest)
-                    if not mem_seg == "static"
-                    else f"{file_stem}.{int(dest)}"
-                )
-                instruction = "\n".join(
-                    [
-                        f"// push {mem_seg} {dest}",
-                        f"@{address}",
-                        "D=M",
-                        "@SP",
-                        "A=M",
-                        "M=D",
-                        INSTRUCTIONS["SP++"],
-                    ]
-                )
-                output.append(instruction)
-            else:
-                instruction = "\n".join(
-                    [
-                        f"// push {mem_seg} {dest}",
-                        f"@{SYMBOLS[mem_seg]}",
-                        "D=M",
-                        f"@{dest}",
-                        "A=D+A",
-                        "D=M",
-                        "@SP",
-                        "A=M",
-                        "M=D",
-                        INSTRUCTIONS["SP++"],
-                    ]
-                )
-                output.append(instruction)
+            output.append(generate_push(line, file_stem))
         elif command == "pop":
-            _, mem_seg, dest = line.split(" ")
-            if mem_seg in ("temp", "pointer", "static"):
-                address = (
-                    MEM_MAP[mem_seg] + int(dest)
-                    if not mem_seg == "static"
-                    else f"{file_stem}.{int(dest)}"
-                )
-                instruction = "\n".join(
-                    [
-                        f"// pop {mem_seg} {dest}",
-                        f"@{address}",
-                        "D=A",
-                        "@R13",
-                        "M=D",
-                        "@SP",
-                        "M=M-1",
-                        "A=M",
-                        "D=M",
-                        "@R13",
-                        "A=M",
-                        "M=D",
-                    ]
-                )
-            else:
-                instruction = "\n".join(
-                    [
-                        f"// pop {mem_seg} {dest}",
-                        f"@{SYMBOLS[mem_seg]}",
-                        "D=M",
-                        f"@{dest}",
-                        "D=D+A",
-                        "@R13",
-                        "M=D",
-                        "@SP",
-                        "M=M-1",
-                        "A=M",
-                        "D=M",
-                        "@R13",
-                        "A=M",
-                        "M=D",
-                    ]
-                )
-            output.append(instruction)
+            output.append(generate_pop(line, file_stem))
         elif command in COMPS:
             output.append(COMPS[command](comp_count))
             comp_count += 1
