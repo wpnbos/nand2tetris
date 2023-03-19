24e914ded72f4642e598759a756bdc9a517a48c1
William <wpnbos@gmail.com>
2023-03-19 20:27:51 +0100

    Working on syntax analyzer

--- projects/10/syntax_analyzer.py
+++ projects/10/syntax_analyzer.py
@@ -76,8 +76,186 @@ def tokenize(program: str) -> list[Token]:
     return tokens
 
 
+@dataclass
+class Statement:
+    statement_token: Token
+
+
+@dataclass
+class IfStatement(Statement):
+    pass
+
+
+@dataclass
+class letStatement(Statement):
+    pass
+
+
+@dataclass
+class whileStatement(Statement):
+    pass
+
+
+@dataclass
+class doStatement(Statement):
+    pass
+
+
+@dataclass
+class returnStatement(Statement):
+    pass
+
+
+@dataclass
+class VariableDeclaration:
+    init_token: Token
+    type_token: Token
+    name_tokens: list[Token]
+    closing_token: Token
+
+
+@dataclass
+class SubroutineBody:
+    variable_declarations: list[VariableDeclaration]
+    statements: list[Statement]
+
+
+@dataclass
+class ClassVarDeclaration:
+    init_token: Token
+    type_token: Token
+    name_tokens: list[Token]
+    closing_token: Token
+
+
+@dataclass
+class SubroutineDeclaration:
+    init_token: Token
+    type_token: Token
+    name_token: Token
+    parameter_list: list[Token]
+    body: SubroutineBody
+
+
+@dataclass
+class Class_:
+    init_token: Token
+    name_token: Token
+    opening_token: Token
+    class_var_declarations: list[ClassVarDeclaration]
+    subroutine_declarations: list[SubroutineDeclaration]
+    closing_token: Token
+
+
+def handle_class_var_declaration(tokens: list[Token]) -> ClassVarDeclaration:
+    remaining_tokens = tokens[2:]
+    name_tokens = []
+    closing_token = None
+    for token in remaining_tokens:
+        if token.text == ";":
+            closing_token = token
+            break
+        name_tokens.append(token)
+    return ClassVarDeclaration(
+        init_token=tokens[0],
+        type_token=tokens[1],
+        name_tokens=name_tokens,
+        closing_token=closing_token,
+    )
+
+
+def handle_variable_declaration(tokens: list[Token]) -> VariableDeclaration:
+    remaining_tokens = tokens[2:]
+    name_tokens = []
+    closing_token = None
+    for token in remaining_tokens:
+        if token.text == ";":
+            closing_token = token
+            break
+        name_tokens.append(token)
+    return VariableDeclaration(
+        init_token=tokens[0],
+        type_token=tokens[1],
+        name_tokens=name_tokens,
+        closing_token=closing_token,
+    )
+
+
+def handle_statement(tokens: list[Token]) -> Statement:
+    pass
+
+
+def handle_subroutine_body(tokens: list[Token]) -> SubroutineBody:
+    variable_declarations = []
+    statements = []
+    for i, token in enumerate(tokens):
+        if token.text == "var":
+            variable_declarations.append(handle_variable_declaration(tokens[i:]))
+        elif token.text in ("let", "if", "while", "do", "return"):
+            # TODO: handle_* functions should also return remaining tokens
+            pass
+        statements.append(handle_statement(tokens[i:]))
+    return SubroutineBody(
+        variable_declarations=variable_declarations,
+        statements=statements,
+    )
+
+
+def handle_subroutine_declaration(tokens: list[Token]) -> SubroutineDeclaration:
+    remaining_tokens = tokens[3:]
+    parameters: list[Token] = []
+    parameter_opening_token = None
+    parameter_closing_token = None
+    for i, token in enumerate(remaining_tokens):
+        if token.text == "(":
+            parameter_opening_token = token
+        elif token.text == ")":
+            parameter_closing_token = token
+            remaining_tokens = remaining_tokens[i:]
+            break
+        parameters.append(token)
+    body = handle_subroutine_body(remaining_tokens)
+    return SubroutineDeclaration(
+        init_token=tokens[0],
+        type_token=tokens[1],
+        name_token=tokens[2],
+        parameter_list=parameters,
+        body=body,
+    )
+
+
+def handle_class_token(tokens: list[Token]) -> Class_:
+    class_var_declarations = []
+    subroutine_declarations = []
+    remaining_tokens = tokens[3:]
+    closing_token = None
+    for i, token in enumerate(remaining_tokens):
+        if token.text in ("static", "field"):
+            # Class variable declaration
+            class_var_declarations.append(
+                handle_class_var_declaration(remaining_tokens[i:])
+            )
+        elif token.text in ("constructor", "function", "method"):
+            # Subroutine declaration
+            subroutine_declarations.append(
+                hande_subroutine_declaration(remaining_tokens[i:])
+            )
+        elif token.text == "}":
+            closing_token = token
+            break
+    return Class_(
+        init_token=tokens[0],
+        name_token=tokens[1],
+        opening_token=tokens[2],
+        class_var_declarations=class_var_declarations,
+        subroutine_declarations=subroutine_declarations,
+        closing_token=closing_token,
+    )
+
+
 def generate_xml(program: str) -> str:
-    return tokenize(program)
+    result = process_tokens(tokenize(program))
+    return result
 
 
 if __name__ == "__main__":
--- projects/10/test_syntax_analyzer.py
+++ projects/10/test_syntax_analyzer.py
@@ -8,14 +8,6 @@ example_programs = ("Square", "ExpressionLessSquare", "ArrayTest")
 example_dirs = [Path(__file__).parent.joinpath(dir_) for dir_ in example_programs]
 
 
-# @pytest.mark.parametrize("program_dir", example_dirs, ids=example_programs)
-# def test_creates_correct_xml(program_dir: Path):
-#     jack_files = program_dir.glob("*.jack")
-#     for jack_file in jack_files:
-#         expected_output = (program_dir / f"{jack_file.stem}T.xml").read_text()
-#         assert syntax_analyzer.generate_xml(jack_file.read_text()) == expected_output
-
-
 def get_jack_files():
     for program_dir in example_dirs:
         for jack_file in program_dir.glob("*.jack"):
@@ -34,3 +26,13 @@ def test_tokenizes_program_correctly(jack_file: Path):
         expected_text = text.split(" <", maxsplit=1)[0].lstrip()
         assert tokenized[i].type_.name == expected_token_type
         assert tokenized[i].text == expected_text
+
+
+@pytest.mark.parametrize("jack_file", get_jack_files())
+def test_creates_correct_xml(jack_file: Path):
+    expected_xml = (
+        jack_file.parent.joinpath(f"{jack_file.stem}.xml").read_text().splitlines()
+    )
+    generated_xml = syntax_analyzer.generate_xml(jack_file.read_text())
+    for i, expected_line in enumerate(expected_xml):
+        assert generated_xml[i] == expected_line
