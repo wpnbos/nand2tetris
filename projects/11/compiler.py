578f23adeb7134c8710b1d3ac34ae09d7437761d
William <wpnbos@gmail.com>
2023-04-02 16:46:32 +0200

    Remove tokenization code to own file

--- projects/11/compiler.py
+++ projects/11/compiler.py
@@ -1,29 +1,10 @@
 from __future__ import annotations
 
-import re
 from collections import defaultdict
 from dataclasses import dataclass
-from enum import Enum, auto
 from typing import Generator, Optional
 
-from lexical_elements import keywords, symbols
-
-STRING_SPACE = "?x?"
-
-
-class TokenType(Enum):
-    keyword = auto()
-    symbol = auto()
-    identifier = auto()
-    stringConstant = auto()
-    integerConstant = auto()
-
-
-@dataclass
-class Token:
-    text: str
-    type_: TokenType
-
+from tokenizer import Token, TokenType, tokenize
 
 kinds = ("static", "field", "var", "arg")
 
@@ -77,34 +58,6 @@ class SymbolTable:
         return f"{self.class_name}\n{rows}"
 
 
-def generate_symbol_table(class_name: str, tokens: list[Token]) -> SymbolTable:
-    symbol_table = SymbolTable(class_name)
-    for i, token in enumerate(tokens):
-        if token.text in ("constructor", "function"):
-            break
-        if not token.type_ == TokenType.identifier:
-            continue
-        if tokens[i + 1].type_ == TokenType.identifier:
-            # This is a class type token
-            continue
-        type_token = tokens[i - 1]
-        if type_token.text == "do":
-            # Identifier is a subroutine name
-            continue
-        if type_token.text == "class":
-            continue
-        if token.text not in symbol_table:
-            # Add symbol to table
-            kind = tokens[i - 2].text
-            if kind not in ("static", "field"):
-                print(token)
-                raise ValueError("Kind is not one of {static, field}")
-            symbol_table.add_symbol(token.text, type_=type_token.text, kind=kind)
-
-    print(symbol_table)
-    return symbol_table
-
-
 class SubroutineTable(SymbolTable):
     def __init__(
         self,
@@ -133,61 +86,6 @@ class SubroutineTable(SymbolTable):
     def __str__(self) -> str:
         rows = "\n".join([str(row) for row in self.table.values()])
         return f"{self.class_name + '.' + self.subroutine_name}\n{rows}"
-
-
-def strip_comments(program: str) -> str:
-    program = re.sub(
-        pattern=r"/\*\*.*?\*/", string=program, repl="", flags=re.MULTILINE | re.DOTALL
-    )
-    return "\n".join(
-        [
-            line.split("//")[0].strip()
-            for line in program.splitlines()
-            if line.strip() and not line.strip().startswith("//")
-        ]
-    ).strip()
-
-
-def tokenize(program: str) -> list[Token]:
-    program = strip_comments(program)
-    fixed_strings = re.sub(
-        pattern=r"(?<=\").+(?=\")",
-        string=program,
-        repl=lambda match: match.group().replace(" ", STRING_SPACE),
-    )
-    left_spaced_program = re.sub(
-        pattern=rf"(?<! )[{''.join([re.escape(symbol) for symbol in symbols])}]",
-        string=fixed_strings,
-        repl=lambda match: " " + match.group(),
-    )
-    spaced_program = re.sub(
-        pattern=rf"[{''.join([re.escape(symbol) for symbol in symbols])}](?! )",
-        string=left_spaced_program,
-        repl=lambda match: match.group() + " ",
-    )
-    tokenized = [
-        token.replace(STRING_SPACE, " ").strip() for token in spaced_program.split()
-    ]
-    tokens = []
-    for token in tokenized:
-        if token in keywords:
-            type_ = TokenType.keyword
-        elif token in symbols:
-            type_ = TokenType.symbol
-        elif token.startswith('"'):
-            type_ = TokenType.stringConstant
-        elif token.isnumeric():
-            type_ = TokenType.integerConstant
-        else:
-            type_ = TokenType.identifier
-        text = (
-            token.replace('"', "")
-            .replace("&", "&amp;")
-            .replace("<", "&lt;")
-            .replace(">", "&gt;")
-        )
-        tokens.append(Token(text=text, type_=type_))
-    return tokens
 
 
 def handle_class_token(
@@ -574,4 +472,6 @@ if __name__ == "__main__":
     target_file = Path.cwd() / sys.argv[1]
     if not target_file.suffix == ".jack":
         raise ValueError(f"File type {repr(target_file.suffix)} not supported")
+    target_file.with_suffix(".vm").write_text(compile_(target_file.read_text()))
+
     target_file.with_suffix(".vm").write_text(compile_(target_file.read_text()))
---
+++ projects/11/tokenizer.py
@@ -0,0 +1,77 @@
+import re
+from dataclasses import dataclass
+from enum import Enum, auto
+
+from lexical_elements import keywords, symbols
+
+
+class TokenType(Enum):
+    keyword = auto()
+    symbol = auto()
+    identifier = auto()
+    stringConstant = auto()
+    integerConstant = auto()
+
+
+@dataclass
+class Token:
+    text: str
+    type_: TokenType
+
+
+STRING_SPACE = "?x?"
+
+
+def strip_comments(program: str) -> str:
+    program = re.sub(
+        pattern=r"/\*\*.*?\*/", string=program, repl="", flags=re.MULTILINE | re.DOTALL
+    )
+    return "\n".join(
+        [
+            line.split("//")[0].strip()
+            for line in program.splitlines()
+            if line.strip() and not line.strip().startswith("//")
+        ]
+    ).strip()
+
+
+def tokenize(program: str) -> list[Token]:
+    program = strip_comments(program)
+    fixed_strings = re.sub(
+        pattern=r"(?<=\").+(?=\")",
+        string=program,
+        repl=lambda match: match.group().replace(" ", STRING_SPACE),
+    )
+    left_spaced_program = re.sub(
+        pattern=rf"(?<! )[{''.join([re.escape(symbol) for symbol in symbols])}]",
+        string=fixed_strings,
+        repl=lambda match: " " + match.group(),
+    )
+    spaced_program = re.sub(
+        pattern=rf"[{''.join([re.escape(symbol) for symbol in symbols])}](?! )",
+        string=left_spaced_program,
+        repl=lambda match: match.group() + " ",
+    )
+    tokenized = [
+        token.replace(STRING_SPACE, " ").strip() for token in spaced_program.split()
+    ]
+    tokens = []
+    for token in tokenized:
+        if token in keywords:
+            type_ = TokenType.keyword
+        elif token in symbols:
+            type_ = TokenType.symbol
+        elif token.startswith('"'):
+            type_ = TokenType.stringConstant
+        elif token.isnumeric():
+            type_ = TokenType.integerConstant
+        else:
+            type_ = TokenType.identifier
+        text = (
+            token.replace('"', "")
+            .replace("&", "&amp;")
+            .replace("<", "&lt;")
+            .replace(">", "&gt;")
+        )
+        tokens.append(Token(text=text, type_=type_))
+    return tokens
