000080e2fb34006c92c014d6ddd19a7f5945bfc1
William <wpnbos@gmail.com>
2023-04-02 16:50:03 +0200

    Move symbol table code to own file

---
+++ projects/11/symbol_table.py
@@ -0,0 +1,87 @@
+from __future__ import annotations
+
+from collections import defaultdict
+from dataclasses import dataclass
+from typing import Generator, Optional
+
+
+@dataclass
+class Symbol:
+    name: str
+    type_: str
+    kind: str
+    index: int
+
+    def __post_init__(self):
+        types = ("int", "boolean", "char")
+        if self.type_ not in types:
+            if not self.type_.title()[0] == self.type_[0]:
+                raise ValueError(f"Type is not one of {types} or a valid class name")
+        if self.kind not in kinds:
+            raise ValueError(f"Kind is not one of {kinds}")
+
+    def __str__(self) -> str:
+        return f"<{self.name},{self.type_},{self.kind},{self.index}>"
+
+
+class SymbolTable:
+    def __init__(
+        self,
+        class_name: str,
+        table: Optional[dict[str, Symbol]] = None,
+        counts: Optional[dict[str, int]] = None,
+    ) -> None:
+        self.class_name = class_name
+        self.table = table or {}
+        self.counts = counts or defaultdict(int)
+
+    def add_symbol(self, name: str, type_: str, kind: str) -> SymbolTable:
+        # print(
+        #     f"Adding symbol {name} with kind {kind}, count for that kind:",
+        #     self.counts[kind],
+        # )
+        self.table[name] = Symbol(
+            name=name, type_=type_, kind=kind, index=self.counts[kind]
+        )
+        self.counts[kind] += 1
+        return SymbolTable(self.class_name, self.table, self.counts)
+
+    def __iter__(self) -> Generator:
+        yield from list(self.table.keys())
+
+    def __str__(self) -> str:
+        rows = "\n".join([str(row) for row in self.table.values()])
+        return f"{self.class_name}\n{rows}"
+
+
+class SubroutineTable(SymbolTable):
+    def __init__(
+        self,
+        parent_table: SymbolTable,
+        subroutine_name: str,
+        table: Optional[dict[str, Symbol]] = None,
+        counts: Optional[dict[str, int]] = None,
+    ) -> None:
+        super().__init__(parent_table.class_name, table, counts)
+        self.subroutine_name = subroutine_name
+        if not self.table:
+            self.add_symbol(name="this", type_=parent_table.class_name, kind="arg")
+        self.parent = parent_table
+
+    def __iter__(self) -> Generator:
+        yield from list(self.table.keys()) + list(self.parent.table.keys())
+
+    def __getitem__(self, key: str) -> str:
+        symbol = self.table.get(key, None) or self.parent.table.get(key, None)
+        if symbol is None:
+            raise KeyError
+        loc = f"{symbol.kind} {symbol.index}"
+        print(f"Accessing {symbol.name} @", loc)
+        return loc
+
+    def __str__(self) -> str:
+        rows = "\n".join([str(row) for row in self.table.values()])
+        return f"{self.class_name + '.' + self.subroutine_name}\n{rows}"
+
+
+kinds = ("static", "field", "var", "arg")
